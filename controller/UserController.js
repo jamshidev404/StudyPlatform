7a7c9c4bc804cbfd8d615d6129318ce2c735e10a
jamshidev404 <awesome-jamkhan@yandex.com>
2021-11-15 19:03:54 +0500

    sdcsfgxC

--- controller/UserController.js
+++ controller/UserController.js
@@ -64,7 +64,7 @@ exports.me = async (req, res, next) => {
 };
 
 exports.getOne = async (req, res, next) => {
-    await User.findOne(req.params.id)
+    await User.findOne({ _id: req.params.id })
     .exec((err, data) => {
         if (err) return res.status(404).json({ success: false, err });
         return res.status(200).json({ success: true, data })
