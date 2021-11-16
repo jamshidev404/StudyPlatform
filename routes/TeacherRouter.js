2a89b24a8e2f2a5a5866b1edf635b7a8dd38f89d
jamshidev404 <awesome-jamkhan@yandex.com>
2021-11-16 15:54:43 +0500

    xsadcf

--- routes/TeacherRouter.js
+++ routes/TeacherRouter.js
@@ -2,10 +2,10 @@ const express  = require("express");
 const router = express.Router();
 const Teacher = require("../controller/TeacherController");
 
-router.post("/add", Group.create),
-router.get("/all", Group.getAll),
-router.get("/:id", Group.getOne),
-router.put("/:id", Group.updateOne),
-router.delete("/:id", Group.rm);
+router.post("/add", Teacher.create),
+router.get("/all", Teacher.getAll),
+router.get("/:id", Teacher.getOne),
+router.put("/:id", Teacher.updateOne),
+router.delete("/:id", Teacher.rm);
 
 module.exports = router;
