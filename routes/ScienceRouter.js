7ddb93260c483c2d180928f9730cd4579424c405
jamshidev404 <awesome-jamkhan@yandex.com>
2021-11-18 14:57:08 +0500

    dsf

--- routes/ScienceRouter.js
+++ routes/ScienceRouter.js
@@ -5,7 +5,7 @@ const Science = require("../controller/ScienceController");
 router.post("/add", Science.create),
 router.get("/all", Science.getAll),
 router.get("/:id", Science.getOne),
-router.get("/science/:id", Science.getScience),
+router.get("/get/:id", Science.getScience),
 router.put("/:id", Science.updateOne),
 router.delete("/:id", Science.rm);
 
