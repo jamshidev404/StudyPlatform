b5981b9711a3bec5f3c69aae831f9a8f51d7e4cb
jamshidev404 <jamshidbek536@gmail.com>
2021-12-08 18:21:43 +0500

    dcsfvg

--- routes/Markazim.js
+++ routes/Markazim.js
@@ -2,6 +2,10 @@ const router = require("express").Router()
 const Markaz = require("../controller/Markazim");
 
 router.post('/add', Markaz.adding);
+router.get('/all', Markaz.getAll);
+router.get('/:id', Markaz.getOne);
+router.put('/:id', Markaz.updateOne);
+router.delete('/:id', Markaz.rm);
 
 
 module.exports = router;
