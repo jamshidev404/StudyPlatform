3f6ff5b56d51f7c2cb6736db713a164a0929e353
jamshidev404 <jamshidbek536@gmail.com>
2021-12-14 15:34:12 +0500

    csdf

--- controller/PayController.js
+++ controller/PayController.js
@@ -37,12 +37,17 @@ exports.create = (req, res) => {
 // };
 
 exports.getAll = async (req, res) => {
+  const { page, limit } = req.query;
+  const count = await Pay.countDocuments();
   await Pay.find({ center_id: req.body.center })
+    .sort({ createdAt: -1 })
+    .skip((page - 1) * limit)
+    .limit(limit * 1)
     .populate({ path: "pupil_id", select: ["name", "phone"] })
     .populate({ path: "group_id", select: "name" })
     .exec((err, data) => {
       if (err) return res.status(404).json({ success: false, err });
-      return res.status(200).json({ success: true, data: data });
+      return res.status(200).json({ success: true, data: data, count });
     });
 };
 
--- controller/ScienceController.js
+++ controller/ScienceController.js
@@ -15,7 +15,7 @@ exports.create = async (req, res) => {
 };
 
 exports.getAll = async (req, res, next) => {
-  const { page = 1, limit = 10 } = req.query;
+  const { page, limit } = req.query;
   const count = await Science.countDocuments();
   await Science.find({ center_id: req.body.center })
     .sort({ createdAt: -1 })
