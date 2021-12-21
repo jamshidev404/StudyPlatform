0e8add5c07b0cb83f80e4853d3f11e94e17cff28
jamshidev404 <jamshidbek536@gmail.com>
2021-12-21 11:07:57 +0500

    qwerty

--- models/ExamModel.js
+++ models/ExamModel.js
@@ -1,33 +1,34 @@
 const mongoose = require("mongoose");
 
-const schema = mongoose.Schema({
+const schema = mongoose.Schema(
+  {
     teacher: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     science: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     startData: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     group: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     center_id: {
-        type: mongoose.Schema.ObjectId,
-        ref: "Markaz",
-        required: true
+      type: mongoose.Schema.ObjectId,
+      ref: "Markaz",
+      required: true,
     },
     room: {
-        type: String,
-        required: true
-    }
-},
-   { timestamps: true }
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Exam", schema);
