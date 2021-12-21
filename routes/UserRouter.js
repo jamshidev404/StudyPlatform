eb084a5d38120ec979243eb84fea0c4a21fd42e2
jamshidev404 <jamshidbek536@gmail.com>
2021-12-21 20:13:24 +0500

    qwerty

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const User = require("../controller/UserController");
 const { protect, authorize } = require("../middleware/auth");
 
-router.post("/add", protect, authorize("admin"), User.create);
+router.post("/add", protect, authorize("superadmin"), User.create);
 router.post("/login", User.login);
 router.get("/me", User.me);
-router.get("/all", protect, authorize("admin"), User.getAll);
-router.get("/:id", protect, authorize("admin"), User.getOne);
-router.put("/:id", protect, authorize("admin"), User.updateOne);
-router.delete("/:id", protect, authorize("admin"), User.deleteOne);
+router.get("/all", protect, authorize("superadmin"), User.getAll);
+router.get("/:id", protect, authorize("superadmin"), User.getOne);
+router.put("/:id", protect, authorize("superadmin"), User.updateOne);
+router.delete("/:id", protect, authorize("superadmin"), User.deleteOne);
 
 module.exports = router;
