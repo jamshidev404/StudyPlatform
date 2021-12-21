5bd37ce261799b6dbc989cb58863030533d467a9
jamshidev404 <jamshidbek536@gmail.com>
2021-12-21 20:25:20 +0500

    qwerty

--- routes/CentersPayRouter.js
+++ routes/CentersPayRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const CenterPay = require("../controller/CentersPayController");
+const { protect, authorize } = require("../middleware/auth");
 
 router.post("/add", CenterPay.create),
-  router.get("/all", CenterPay.getAll),
+  router.get("/all", protect, authorize("superadmin"), CenterPay.getAll),
   router.get("/by/:id", CenterPay.getOnes),
   router.get("/:id", CenterPay.getOne),
   router.put("/:id", CenterPay.updateOne),
