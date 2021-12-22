b89611a583bc65555a2946b14a98431b4675ee18
jamshidev404 <jamshidbek536@gmail.com>
2021-12-22 20:39:24 +0500

    qwerty

--- routes/StatistikaRouter.js
+++ routes/StatistikaRouter.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 const Statitika = require("../controller/StatistikaController");
+const { protect, authorize } = require("../middleware/auth");
 
-router.get("/all", Statitika.getAll);
+router.get(
+  "/all",
+  protect,
+  authorize("superadmin", "admin", "moderator"),
+  Statitika.getAll
+);
 
 module.exports = router;
